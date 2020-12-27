2460d9b54824c6a617a2f9636723722c3b17f1a7
Shendy Krisyohanda <75378901+krisyohanda@users.noreply.github.com>
2020-12-27 17:47:11 +0800

    Add files via upload

---
+++ R2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+    int R1, R2, S;
+    scanf("%d", &R1);
+    scanf("%d", &S);
+    if (R1,S>=-1000 && R1,S<=1000)
+    {
+        R2=2*S-R1;
+        printf("%d", R2);
+    }
+    return 0;
+}
